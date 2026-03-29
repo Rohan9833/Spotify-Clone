d197c42747462450a8d05080a9902f93479edf55
Rohan Pal <rohanpal9833@gmail.com>
2026-03-29 12:29:10 +0530

    installed axios

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,12 @@ const DB = require("./src/config/DB")
 const app = require("./src/app")
 DB();
 
-app.listen(3000,()=>{
-    console.log("your server is running on port 3000")
-})
+// app.listen(3000,()=>{
+//     console.log("your server is running on port 3000")
+// })
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
